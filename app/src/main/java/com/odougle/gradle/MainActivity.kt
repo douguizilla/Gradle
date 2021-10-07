b47aa5fae9ed1858d73a2b50a23fd132d7622873
Douglas Gomes <douglasgommez@gmail.com>
2021-10-06 22:02:49 -0300

    set button click to start activity intent going to LibActivity

--- app/src/main/java/com/odougle/gradle/MainActivity.kt
+++ app/src/main/java/com/odougle/gradle/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.odougle.gradle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.gradle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            startActivity(Intent(this, LibActivity::class.java))
+        }
     }
 }
